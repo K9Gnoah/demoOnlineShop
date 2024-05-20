5d0b0f38e33f2e32022aa4e9ef55dbba19cea2d2
K9Gnoah <hoangdominohotboizbg@gmail.com>
2024-05-20 08:46:46 +0700

    Create Project with startspring.io

---
+++ demeOnlineShop/src/main/java/com/hoangmike/demeOnlineShop/DemeOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.hoangmike.demeOnlineShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemeOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemeOnlineShopApplication.class, args);
+	}
+
+}
---
+++ demeOnlineShop/src/test/java/com/hoangmike/demeOnlineShop/DemeOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangmike.demeOnlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
